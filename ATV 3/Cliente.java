6877cb48d3a72e85f4b97301a1518b012d49b8d4
tolbarroso <carol.00000829955@unicap.br>
2023-05-25 18:48:27 -0300

    commit

--- ATV 3/Cliente.java
+++ ATV 3/Cliente.java
@@ -95,7 +95,7 @@ public class Cliente implements Operacoes {
         }
     }
 
-    private void atualizarHistorico(Operacao operacao) {
+    protected void atualizarHistorico(Operacao operacao) {
         for (int i = historico.length - 1; i > 0; i--) {
             historico[i] = historico[i - 1];
         }
